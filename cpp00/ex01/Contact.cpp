fb5d08aab28f7c1f2b574a8afff261ba17c623ae
Erick Gomes <egomes@c1r14s6.42lisboa.com>
2022-03-03 20:06:30 +0000

    42

--- cpp00/ex01/Contact.cpp
+++ cpp00/ex01/Contact.cpp
@@ -1,26 +1,13 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   phonebook.hpp                                      :+:      :+:    :+:   */
+/*   contact.cpp                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: egomes <egomes@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/03/03 19:29:36 by egomes            #+#    #+#             */
-/*   Updated: 2022/03/03 20:04:41 by egomes           ###   ########.fr       */
+/*   Created: 2022/03/03 19:52:14 by egomes            #+#    #+#             */
+/*   Updated: 2022/03/03 20:04:05 by egomes           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PHONE_BOOK_HPP
-# define PHONE_BOOK_HPP
-
 # include "Contact.hpp"
-
-class PhoneBook {
-private:
-    int i;
-public:
-	Contact	contact[8];
-    void PhoneBook::addContact();
-};
-
-#endif
--- cpp00/ex01/Contact.hpp
+++ cpp00/ex01/Contact.hpp
@@ -1,26 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   phonebook.hpp                                      :+:      :+:    :+:   */
+/*   Contact.hpp                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: egomes <egomes@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/03/03 19:29:36 by egomes            #+#    #+#             */
-/*   Updated: 2022/03/03 20:04:41 by egomes           ###   ########.fr       */
+/*   Created: 2022/03/03 20:03:23 by egomes            #+#    #+#             */
+/*   Updated: 2022/03/03 20:04:08 by egomes           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PHONE_BOOK_HPP
-# define PHONE_BOOK_HPP
+#ifndef CONTACT_HPP
+# define CONTACT_HPP
 
-# include "Contact.hpp"
+# include <iostream>
+# include <string.h>
 
-class PhoneBook {
-private:
-    int i;
-public:
-	Contact	contact[8];
-    void PhoneBook::addContact();
+class Contact {
+	private:
+	std::string	f_name;
+	std::string	l_name;
+	std::string	n_name;
+	std::string	phone_num;
+	std::string	dark_secr;
+	std::string	index;
 };
 
 #endif
